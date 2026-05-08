483a974c77f6bf67d1ec2c893719fbf42fc102d8
Noah Kara <s4nokarani-trier.de>
2026-05-08 11:39:33 +0200

    chnage on main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
-        Car car = new Car("red", "VW", 5);
+        Car car = new Car("yellow", "VW", 5);
         Car car1 = new Car("blue", "Audi", 3);
 
         factory.buildCar(car);
